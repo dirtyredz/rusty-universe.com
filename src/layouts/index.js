15137f91939a8d7e99f2f868f3e445a024f87319
dirtyredz <dirtyredz@live.com>
2018-02-03 13:03:13 -0600

    Failing on importing index.css?

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import ScrollUpButton from 'react-scroll-up-button'
-import AwesomeBackground from '../components/AwesomeBackground'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
+
 import './index.css'
 
 const TemplateWrapper = ({ children }) => (

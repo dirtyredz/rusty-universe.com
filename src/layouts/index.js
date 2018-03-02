d9d0de0fde9f1e708af797722c54cd431d013e72
dirtyredz <dirtyredz@live.com>
2018-03-01 21:05:21 -0600

    Remove Preloader

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,11 +5,15 @@ import ScrollUpButton from 'react-scroll-up-button'
 import AwesomeBackground from '../components/AwesomeBackground'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-import styled from 'styled-components'
+import styled, { injectGlobal } from 'styled-components'
 import './index.css'
 import WidgetBot from '../components/WidgetBot'
 import galaxy from '../components/resources/galaxy.jpg'
-
+import TallLean from '../components/resources/Tall & Lean.ttf'
+import Borg from '../components/resources/Borg.ttf'
+import Elixia from '../components/resources/ELIXIA.ttf'
+import EuroStyle from '../components/resources/EUROS3.ttf'
+import FontFaceObserver from 'fontfaceobserver'
 
 const Wrapper = styled.div`
     min-height: 100%;
@@ -21,29 +25,32 @@ const Wrapper = styled.div`
 class TemplateWrapper extends React.Component{
     constructor(props){
         super(props)
-        this.state={Loaded: false}
+        this.state={Loaded: true}
     }
     onLoaded(){
-        this.setState({Loaded: true})
+        //this.setState({Loaded: true})
+    }
+    componentDidMount(){
+
     }
     render(){
         if(this.state.Loaded){
             return(
 
                 <Wrapper>
-                  <AwesomeBackground/>
-                  <ScrollUpButton/>
-                  <Helmet
-                    title="Rusty-Universe"
-                    meta={[
-                      { name: 'description', content: 'Sample' },
-                      { name: 'keywords', content: 'sample, something' },
-                    ]}
-                  />
-                  <Header />
-                  {this.props.children()}
-                  <WidgetBot/>
-                  <Footer />
+                    <Helmet
+                      title="Rusty-Universe"
+                      meta={[
+                        { name: 'description', content: 'Sample' },
+                        { name: 'keywords', content: 'sample, something' },
+                      ]}
+                    />
+                    <AwesomeBackground/>
+                    <Header />
+                    {this.props.children()}
+                    <Footer />
+                    <WidgetBot/>
+                    <ScrollUpButton/>
               </Wrapper>
             )
         }else{
